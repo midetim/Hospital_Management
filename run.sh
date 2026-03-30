2577edb9af62efd3055b4f2378ffd996f8e1ad73
Matthew Hvizdos <matthewhvizdos@gmail.com>
2026-03-30 00:52:21 -0400

    last commit

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Step 1 -- Check input
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 [cli|gui]"
+    echo -e "\033[31mUsage: $0 [cli|gui]\033[0m"
     exit 1
 fi
 
 MODE=$1
 
 # Step 1 -- Docker Compose
-echo "Building and starting Docker Compose"
+echo -e "\033[36mBuilding and starting Docker Compose...\033[0m"
 docker compose down
 docker compose up --build -d
-echo "Docker Compose setup complete"
+echo -e "\033[32mDocker Compose setup complete.\033[0m"
 
 # Step 2 -- Launch mode
 case "$MODE" in
@@ -20,16 +20,16 @@ case "$MODE" in
         # Replace 'your_container_name' with the actual container name
         CONTAINER=$(docker compose ps -q frontend_cli)
         if [ -z "$CONTAINER" ]; then
-            echo "Error: container for service 'frontend_cli' not found!"
+            echo -e "\033[31mError: container for service 'frontend_cli' not found!\033[0m"
             exit 1
         else
-            echo "Attaching to container $CONTAINER..."
+            echo -e "\033[32mAttaching to container $CONTAINER...\033[0m"
         fi
         docker attach "$CONTAINER"
         ;;
     gui)
         URL="http://localhost:8920"
-        echo "Opening GUI at $URL..."
+        echo -e "\033[32mOpening GUI at \033[35m$URL...\033[0m"
         # macOS
         if [[ "$OSTYPE" == "darwin"* ]]; then
             open "$URL"
@@ -37,11 +37,11 @@ case "$MODE" in
         elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
             xdg-open "$URL"
         else
-            echo "Please open $URL manually in your browser at localhost:8920."
+            echo -e "\033[33mPlease open \033[35m$URL\033[33m manually in your browser.\033[0m"
         fi
         ;;
     *)
-        echo "Invalid mode: $MODE. Use 'cli' or 'gui'."
+        echo -e "\033[31mInvalid mode: $MODE. Use 'cli' or 'gui'.\033[0m"
         exit 1
         ;;
 esac
